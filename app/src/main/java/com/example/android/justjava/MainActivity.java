2a241befab9a013f0818ac9e5155206611d02fd3
rsilverst <rsilverst.commerce@gmail.com>
2015-12-21 23:50:44 -0800

    Add a bunch of new functionality: Scrolling Customer Name field Toppings Checkboxes

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -3,7 +3,10 @@ package com.example.android.justjava;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import java.text.NumberFormat;
@@ -20,6 +23,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public String getName() {
+
+        EditText customerNameField = (EditText) findViewById(R.id.customer_name);
+        String customerName = customerNameField.getText().toString();
+        return customerName;
     }
 
     /**
@@ -42,18 +52,36 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
+        String customerName = getName();
         int price = calculatePrice();
-        String priceMessage = createOrderSummary(price);
+        CheckBox whippedCreamCheckbox = (CheckBox) findViewById(R.id.whipped_cream);
+        boolean hasWhippedCream = whippedCreamCheckbox.isChecked();
+        CheckBox chocolateCheckbox = (CheckBox) findViewById(R.id.chocolate);
+        boolean hasChocolate = chocolateCheckbox.isChecked();
+        String priceMessage = createOrderSummary(customerName, price, hasWhippedCream, hasChocolate);
         displayMessage(priceMessage);
     }
 
     /**
      *
      * @param price the price of the coffee
+     * @param hasWhippedCream says whether the coffee should have whipped cream or not
+     * @param hasChocolate says whether the coffee should have chocolate or not
      * @return summary
      */
-    private String createOrderSummary(int price) {
-        return "Name: Kaptain Kunal" + "\nQuantity: " + quantity + "\nTotal: $" + price + "\nThank you!";
+    private String createOrderSummary(String name, int price, boolean hasWhippedCream, boolean hasChocolate) {
+
+        String addWhippedCream = "No";
+        String addChocolate = "No";
+        if (hasWhippedCream == true) addWhippedCream = "Yes";
+        if (hasChocolate == true) addChocolate = "Yes";
+        String summaryMessage = "Name: " + name;
+        summaryMessage += "\nAdd whipped cream? " + addWhippedCream;
+        summaryMessage += "\nAdd chocolate? " + addChocolate;
+        summaryMessage += "\nQuantity: " + quantity;
+        summaryMessage += "\nTotal: $" + price;
+        summaryMessage += "\nThank you!";
+        return summaryMessage;
     }
     /**
      *
